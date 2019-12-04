aad731c98016432373e543d3020357bedc2f7a28
nbraunsc <nbraunsc@vt.edu>
2019-12-04 14:57:50 -0500

    added coeff

--- src/tools.py
+++ src/tools.py
@@ -2,6 +2,7 @@ def recurse(f_old, start, derivs, fraglist):
     for fj in range(start+1, len(fraglist)):
         if fj > start:
             df_new = fraglist[fj].intersection(f_old)
+            #df_new.coeff = f_old.coeff * -1
             if len(df_new) > 0:
                 derivs.append(df_new)
                 print(derivs, 'derivs')
@@ -11,6 +12,7 @@ def runpie(fraglist):
     derivs = []
     for fi in range(0, len(fraglist)):
         dfi = fraglist[fi]
+        #dfi.coeff = 1
         derivs.append(dfi)
         recurse(derivs[fi], fi, derivs, fraglist)
     return derivs
