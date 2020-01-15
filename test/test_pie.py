6c2644378fb28c8d48528e48343ec9930e6fc6bd
nbraunsc <nbraunsc@vt.edu>
2020-01-15 15:27:19 -0500

    updated test

--- test/test_pie.py
+++ test/test_pie.py
@@ -7,31 +7,31 @@ def test_pie():
     This is testing if the derivative recursive function works
     """
     aspirin = Molecule()
-    aspirin.initalize_molecule()
+    aspirin.initalize_molecule('aspirin')
     frag = Fragmentation(aspirin)
-    frag.do_fragmentation(1)
-    assert(runpie(frag.frag)[0] == [{1, 3, 4, 5, 6}, {6}, {6}, {1, 4}, {1}, {1, 6}, {2, 6, 7}, {2, 6}, {0, 1, 4}, {1}, {1, 2, 6}])
+    frag.do_fragmentation(1, 'RHF', 'sto-3g')
+    assert(runpie(frag.unique_frag)[0] == [{1, 3, 4, 5, 6}, {6}, {6}, {1, 4}, {1}, {1, 6}, {2, 6, 7}, {2, 6}, {0, 1, 4}, {1}, {1, 2, 6}])
 
     """ coefficent test """
-    assert(runpie(frag.frag)[1] == [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1])
+    assert(runpie(frag.unique_frag)[1] == [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1])
 
 def test_energy():
     """
     This is testing the overall energy by taking energies of fragments multiplied by their coefficents and then added up.  Should be the same energy as the full  molecule
     """
     aspirin = Molecule()
-    aspirin.initalize_molecule()
+    aspirin.initalize_molecule('aspirin')
     frag = Fragmentation(aspirin)
-    frag.do_fragmentation(1)
+    frag.do_fragmentation(1, 'RHF', 'sto-3g')
     print(frag.total)
     assert(frag.total == -636.6280880465249)
     
 
 if __name__ == "__main__":
     aspirin = Molecule()
-    aspirin.initalize_molecule()
+    aspirin.initalize_molecule('aspirin')
     frag = Fragmentation(aspirin)
-    frag.do_fragmentation(1)
+    frag.do_fragmentation(1, 'RHF', 'sto-3g')
     test_pie()
     test_energy()
     
