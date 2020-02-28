3c1804f245a9b83414925c49b3d2be3cc4cef61b
Nicole Mary Braunscheidel <nbraunsc@nrlogin1.cluster>
2020-02-28 11:01:13 -0500

    changed name

--- inputs/runfile.py
+++ inputs/runfile.py
@@ -1,5 +1,5 @@
 #this is where i will need to import my package, need to check name
-from nicole-fragment import *
+from MIM_fragment import *
 
 
 #This will be the file that i initalize all objects and tell it to run.  Ican do multiple runs in this file and compare at the same time!
