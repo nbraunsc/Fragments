992648a0a39fbc7db9ad9295564e18f41042f38b
Nicole Mary Braunscheidel <nbraunsc@nrlogin1.cluster>
2020-05-07 14:22:14 -0400

    runfile

--- inputs/frag_practice.py
+++ inputs/frag_practice.py
@@ -8,20 +8,46 @@ from pyscf import gto, scf, ci, cc, mp, hessian, lib, grad
 from pyscf.geomopt.berny_solver import optimize
 from berny import Berny, geomlib
 
+import os
+import sys
+from pathlib import Path
+
 frag_list = [1, 2, 3, 4, 5]
 ratio_list = []
+molecule_classlist = ["alcohols", "amines"]
 
-for k in frag_list:
-    start_time = time.time()
-    largermol = Molecule.Molecule()
-    largermol.initalize_molecule('largermol')
-    energy_MIM = MIM.do_MIM1(k, 'RHF', 'sto-3g', largermol, 'largermol')[0]
+
+#directory = os.fsencode(str(/home/nbraunsc/Projects/Fragments/inputs/))
+#os.path.abspath(os.curdir)
+
+current_dir = os.getcwd()
+
+for i in molecule_classlist:
+    #directory = os.chdir(i)
+    directory = current_dir + "/" + i + "/"
+    x = 0
+    for file_name in os.listdir(directory):
+        if file_name.endswith(".cml"):
+            for k in frag_list:
+                start_time = time.time()
+                filename_nocml = Path(file_name).stem
+                filename_nocml = Molecule.Molecule(i)
+                filename_nocml.initalize_molecule(Path(file_name).stem) #no cml extension
+                energy_MIM = MIM.do_MIM1(k, 'RHF', 'sto-3g', filename_nocml, Path(file_name).stem)[0]
+                full_energy = runpyscf.do_pyscf(filename_nocml.atomtable, 'RHF', 'sto-3g', hess=False)[0]       #start of full molecule calc
+                total_time = time.time() - start_time
+                energy_error = abs(full_energy - energy_MIM)/full_energy
+                ratio = total_time/energy_error
+                ratio_list.append(ratio)
+            x = np.argmin(ratio_list)      #smaller time to error = best fragmentation level
+        else:
+            continue
     
-    full_energy = runpyscf.do_pyscf(largermol.atomtable, 'RHF', 'sto-3g', hess=False)[0]
-    total_time = time.time() - start_time
-    energy_error = abs(full_energy - energy_MIM)/full_energy
-    ratio = total_time/energy_error
-    ratio_list.append(ratio)
+    os.chdir("../")     #writing results for each molecule class
+    f = open("molecule_class", "a")
+    f.write(i)
+    f.write("Best k value =")
+    f.write(x)
+    f.close()
 
-x = np.argmin(ratio_list)       #smaller time to error = best fragmentation level
-print("Best k value for clustering:", x)
+
