ea7273c85cf6cafab7991b9395643ce20c738d8e
Nicole Mary Braunscheidel <nbraunsc@nrlogin1.cluster>
2020-05-09 17:07:43 -0400

    frag submit script

--- inputs/frag_practice.py
+++ inputs/frag_practice.py
@@ -1,6 +1,6 @@
 import nicolefragment
 from nicolefragment import *
-from nicolefragment import Molecule, MIM, runpyscf
+from nicolefragment import Molecule, MIM, runpyscf, fragmentation
 import time
 import numpy as np
 
@@ -9,8 +9,9 @@ import sys
 from pathlib import Path
 
 frag_list = [1, 2, 3, 4, 5]
-ratio_list = []
-molecule_classlist = ["alcohols", "amines"]
+best_list = []
+molecule_classlist = ["drugs"]
+filenames = []
 
 
 #directory = os.fsencode(str(/home/nbraunsc/Projects/Fragments/inputs/))
@@ -18,35 +19,39 @@ molecule_classlist = ["alcohols", "amines"]
 
 current_dir = os.getcwd()
 
-for i in molecule_classlist:
-    #directory = os.chdir(i)
-    directory = current_dir + "/" + i + "/"
-    x = 0
-    for file_name in os.listdir(directory):
-        if file_name.endswith(".cml"):
-            for k in frag_list:
-                start_time = time.time()
-                filename_nocml = Path(file_name).stem
-                z = str(Path(file_name).stem)
-                a_k = Molecule.Molecule(i)
-                print(a_k)
-                a_k.initalize_molecule(z) #no cml extension
-                print(a_k.atomtable)
-                energy_MIM = MIM.do_MIM1(k, 'RHF', 'sto-3g', a_k, z)[0]
-                full_energy = runpyscf.do_pyscf(a_k.atomtable, 'RHF', 'sto-3g', hess=False)[0]       #start of full molecule calc
-                total_time = time.time() - start_time
-                energy_error = abs(full_energy - energy_MIM)/full_energy
-                ratio = total_time/energy_error
-                ratio_list.append(ratio)
-            x = np.argmin(ratio_list)      #smaller time to error = best fragmentation level
-        else:
-            continue
+#for i in molecule_classlist:
+#directory = os.chdir(i)
+directory = current_dir + "/drugs/"
+x = 0
+for file_name in os.listdir(directory):
+    filenames.append(file_name)
+    ratio_list = []
+    if file_name.endswith(".cml"):
+        for k in frag_list:
+            start_time = time.time()
+            filename_nocml = Path(file_name).stem
+            z = str(Path(file_name).stem)
+            a_k = Molecule.Molecule("drugs")
+            a_k.initalize_molecule(z) #no cml extension
+            frag = fragmentation.Fragmentation(a_k)
+            energy_MIM = frag.do_fragmentation(k, 'RHF', 'sto-3g', z)[0]
+            full_energy = runpyscf.do_pyscf(a_k.atomtable, 'RHF', 'sto-3g', hess=False)[0]       #start of full molecule calc
+            total_time = time.time() - start_time
+            energy_error = abs(full_energy - energy_MIM)/full_energy
+            ratio = total_time/energy_error
+            ratio_list.append(ratio)
+        k_index = np.argmin(ratio_list)
+        best_list.append(k_index)
     
-    os.chdir("../")     #writing results for each molecule class
-    f = open("molecule_class", "a")
-    f.write(i)
-    f.write("Best k value =")
-    f.write(x)
-    f.close()
+
+for i in range(0, len(filenames)):
+    print(filenames[i], "", "Best k value:", "", frag_list[best_list[i]])
+
+#x = np.argmin(ratio_list)      #smaller time to error = best fragmentation level
+f = open("molecule_class", "a")
+f.write("Drugs\n")
+f.write("Best k value =")
+f.write(x)
+f.close()
 
 
